8779eedb3637dbf4e3c53bcc62a776eafb48048f
sanjayaksaxena <sanjayaksaxena@yahoo.com>
2017-10-01 18:52:16 +0530

    feat(string-soundex): add soundex distance

---
+++ src/string-soundex.js
@@ -0,0 +1,49 @@
+//     wink-distance
+//     Distance functions for Bag of Words, Strings,
+//     Vectors and more.
+//
+//     Copyright (C) 2017  GRAYPE Systems Private Limited
+//
+//     This file is part of “wink-distance”.
+//
+//     “wink-distance” is free software: you can redistribute
+//     it and/or modify it under the terms of the GNU Affero
+//     General Public License as published by the Free
+//     Software Foundation, version 3 of the License.
+//
+//     “wink-distance” is distributed in the hope that it will
+//     be useful, but WITHOUT ANY WARRANTY; without even
+//     the implied warranty of MERCHANTABILITY or FITNESS
+//     FOR A PARTICULAR PURPOSE.  See the GNU Affero General
+//     Public License for more details.
+//
+//     You should have received a copy of the GNU Affero
+//     General Public License along with “wink-distance”.
+//     If not, see <http://www.gnu.org/licenses/>.
+
+var soundex = require( 'wink-nlp-utils' ).string.soundex;
+// ## string
+
+// ### jaro
+/**
+ *
+ * Computes the soundex distance between two strings. This distance is either
+ * 1 indicating phonetic similarity or 0 indicating no similarity.
+ *
+ * @name string.soundex
+ * @param {string} str1 — first string.
+ * @param {string} str2 — second string.
+ * @return {number} soundex distance between `str1` and `str2`.
+ * @example
+ * soundex( 'Burroughs', 'Burrows' );
+ * // ->  1
+ * soundex( 'Ekzampul', 'example' );
+ * // -> 1
+ * soundex( 'sat', 'urn' );
+ * // -> 0
+ */
+var soundexDistance = function ( str1, str2 ) {
+  return ( soundex( str1 ) === soundex( str2 ) ) ? 1 : 0;
+}; // soundexDistance()
+
+module.exports = soundexDistance;
